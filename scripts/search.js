4207da924c81533a5a876e63c63c26132ec9212c
AgencyShishova <87063494+AgencyShishova@users.noreply.github.com>
2021-07-08 13:30:32 +0300

    удалена папка скриптов

--- scripts/search.js
+++
@@ -1,5 +0,0 @@
-let navLinkSearch = document.querySelector('.b-header__navLinkSearch');
-let input = document.querySelector('.b-header__input');
-navLinkSearch.addEventListener('click', () => {
-    input.classList.toggle('hidden');
-});
